88148bd475cba5d92800d6d94ca6bb042d78bec4
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-05-22 21:24:41 -0400

    Added followed person component

--- frontend/src/components/FollowList.jsx
+++ frontend/src/components/FollowList.jsx
@@ -1,60 +1,29 @@
 import React from "react";
 import css from "../styles/followList.module.css";
+import OneFollowPerson from "./OneFollowPerson";
 
 const FollowList = () => {
   return (
     <div className={css.layout}>
       <h3 className={css.listHeader}>Follow List</h3>
       <hr className={css.listHeaderBorder} />
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
-      <p>User</p>
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
+      <OneFollowPerson />
     </div>
   );
 };
